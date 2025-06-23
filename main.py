c53f932b7ced89aa4ea7b243234a6942b7642339
Niranjana Ajay <ajayniranjana05@gmail.com>
2025-06-24 03:48:43 +0530

    made more fun with giphy

--- main.py
+++ main.py
@@ -6,25 +6,26 @@ app = Flask(__name__)
 COLOURS = ['violet','indigo','blue','green','yellow','orange','red']
 RESULT = random.randint(0,9)
 
-def random_colour(function,**kwargs):
-    def wrapper(**kwargs):
-        ran_colour = random.choice(COLOURS)
-        return f'<h1 style="color:{ran_colour}">{function(**kwargs)}</h1>'
-    return wrapper
-
 @app.route('/')
 def home_page():
-    return "<h1>Guess a number between 0 and 9 ;)</h1>"
+    return ("<h1>Guess a number between 0 and 9 ;)</h1>"
+            "<img src='https://media3.giphy.com/media/v1.Y2lkPTc5MGI3NjExeGdiZ29naHl3N2ZxNnljMzY4aGkzMTdkbW"
+            "5kY2tsZnF4bzUzajFpZSZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/MDJ9IbxxvDUQM/giphy.gif'>")
 
 @app.route("/<int:num>")
-@random_colour
 def guess_page(num):
+    ran_colour = random.choice(COLOURS)
     if num == RESULT:
-        return "YOU'VE GUESSED IT RIGHT! :)"
+        return (f"<h1 style='color:{ran_colour}'>YOU FOUND MEEE! :)</h1>"
+                "<img src='https://media.giphy.com/media/v1.Y2lkPTc5MGI3NjExc3l2dDcxNTNrbW45N3dlaW9ham55MTN"
+                "iNnY4dDZ1cXMycTYzdmxrdSZlcD12MV9naWZzX3NlYXJjaCZjdD1n/BMR4cgypuglVu/giphy.gif'>")
     elif num > RESULT:
-        return "TOO HIGH! :("
+        return (f"<h1 style='color:{ran_colour}'>TOO HIGH, TRY AGAIN! :(</h1>"
+                "<img src='https://media.giphy.com/media/v1.Y2lkPTc5MGI3NjExNWUxZHJ6ZjNsNmZrdm54ZGJ2c2R2anl"
+                "idHc2MGlkeGtmODhha3J1dCZlcD12MV9naWZzX3NlYXJjaCZjdD1n/yFQ0ywscgobJK/giphy.gif'>")
     else:
-        return "TOO LOW! :("
-
+        return (f"<h1 style='color:{ran_colour}'>TOO LOW, TRY AGAIN! :(</h1>"
+                "<img src='https://media.giphy.com/media/v1.Y2lkPTc5MGI3NjExNWUxZHJ6ZjNsNmZrdm54ZGJ2c2R2anl"
+                "idHc2MGlkeGtmODhha3J1dCZlcD12MV9naWZzX3NlYXJjaCZjdD1n/yFQ0ywscgobJK/giphy.gif'>")
 
 app.run(debug=True)
